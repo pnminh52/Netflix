a7274f310e8ee39b5bd9989fef39b03050347af7
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-12-30 18:02:31 +0700

    ..

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Button, FormControl, FormLabel, Heading, Input, Stack, Text, useToast } from "@chakra-ui/react";
+import {
+  Icon,
+  Flex,
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../services/firebase"; // Adjust the path if needed
 
@@ -18,7 +30,7 @@ const Login = () => {
     try {
       // Sign in using Firebase Authentication
       await signInWithEmailAndPassword(auth, email, password);
-          navigate("/homepage");
+      navigate("/homepage");
       // Show success toast
       toast({
         title: "Login Successful",
@@ -43,7 +55,7 @@ const Login = () => {
 
   return (
     <Box maxW="md" mx="auto" mt="10" p="4" borderWidth="1px" borderRadius="md">
-      <Text >Login Page</Text>
+      <Text>Login Page</Text>
       <form onSubmit={handleSubmit}>
         <Stack spacing={4}>
           <FormControl id="email" isRequired>
@@ -66,9 +78,9 @@ const Login = () => {
           </FormControl>
           <Button
             color="white"
-        bg={'red'}
-        _hover={'none'}
-        _active={'none'}
+            bg={"red"}
+            _hover={"none"}
+            _active={"none"}
             isLoading={loading}
             type="submit"
             loadingText="Logging in"
@@ -77,9 +89,47 @@ const Login = () => {
           </Button>
         </Stack>
       </form>
+      <Box>
+        Or login with:
+        <Flex>
+          <Icon
+            as="svg"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="40px"
+            height="40px"
+            viewBox="0 0 48 48"
+            borderRadius="50%"
+            bg="white"
+            p="8px"
+          >
+            <path
+              fill="#FFC107"
+              d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+            ></path>
+            <path
+              fill="#FF3D00"
+              d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+            ></path>
+            <path
+              fill="#4CAF50"
+              d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+            ></path>
+            <path
+              fill="#1976D2"
+              d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+            ></path>
+          </Icon>
+        </Flex>
+      </Box>
       <Text mt={4}>
         Don't have an account?{" "}
-        <Button variant="link" color="red" onClick={() => navigate("/register")}>
+        <Button
+          variant="link"
+          color="red"
+          onClick={() => navigate("/register")}
+        >
           Register here
         </Button>
       </Text>
