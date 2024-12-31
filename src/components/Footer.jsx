ba55aa3788d85cc810c45100a52c026468a73995
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-12-31 09:17:25 +0700

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,144 @@
-import React from 'react';
-import { Box, Text, Link, Stack, Container } from '@chakra-ui/react';
+import React from "react";
+import { Box, Text, Link, Stack, Container } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Container maxW={"container.xl"}
-      as="footer"
-      bg="black"
-      color="white"
-      
-      textAlign="center"
-    >
-        {/* <Text mt={2}>&copy; {new Date().getFullYear()} Netflix. All copyrights reserved.</Text>
-        <Text mb={4} cursor={'pointer'} color="red.500">Elyz.thedev@gmail.com</Text> */}
-  
+    <Container mt={0} maxW={"container.xl"} as="footer">
+      <Box display={"grid"} gridTemplateColumns="repeat(4,1fr)" gap={4}>
+        <Box>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            FAQ
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Investor Relations
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Privacy
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Speed Test
+          </Text>
+        </Box>
+        <Box>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Help Center
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Jobs
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          ></Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Cookie Preferences
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Legal Notices
+          </Text>
+        </Box>
+        <Box>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Account
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Ways to Watch
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          ></Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Corporate Information
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Only on Netflix
+          </Text>
+        </Box>
+        <Box>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Media Center
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Terms of Use
+          </Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          ></Text>
+          <Text
+            cursor={"pointer"}
+            textDecoration={"underline"}
+            color={"gray.300"}
+          >
+            Contact Us
+          </Text>
+        </Box>
+      </Box>
+      <Box>
+        <Text>
+          Netflix Vietnam
+        </Text>
+      </Box>
     </Container>
   );
 };
