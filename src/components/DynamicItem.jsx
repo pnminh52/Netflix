d0d0d9132c05036e6bc472113a0cb6ff3a9ff85b
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-01-02 16:31:30 +0700

    UI Update

--- src/components/DynamicItem.jsx
+++ src/components/DynamicItem.jsx
@@ -3,12 +3,13 @@ import { Button, Box, Container } from "@chakra-ui/react";
 
 const DynamicItem = () => {
   const [showButton, setShowButton] = useState(false);
-
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   const handleScroll = () => {
     const footer = document.getElementById("dynamic");
     const footerPosition = footer.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
-
     if (footerPosition < windowHeight) {
       setShowButton(true);
     } else {
@@ -38,26 +39,38 @@ const DynamicItem = () => {
           boxShadow="0 0 50px 50px rgba(0, 0, 0, 0.6)"
           display={{ base: "block", sm: "block", md: "none", lg: "none" }}
         >
+          <Box
+            position="absolute"
+            bottom="0"
+            left="0"
+            width="full"
+            height="full"
+            backgroundColor="black"
+            opacity="1"
+            zIndex="-1"
+            rounded={"md"}
+          />
           <Button
-            rounded={"sm"}
+            rounded={"md"}
             _hover={{ bg: "#b20710" }}
             bg="#E50914"
             size="md"
             width="full"
             fontSize={"md"}
             fontWeight={"normal"}
+            onClick={handleClick}
           >
             Get Started
           </Button>
           <Box
-    position="absolute"
-    bottom="-20px"
-    left="0"
-    width="full"
-    height="20px" 
-    backgroundColor="black" 
-    opacity="1" 
-  />
+            position="absolute"
+            bottom="-20px"
+            left="0"
+            width="full"
+            height="20px"
+            backgroundColor="black"
+            opacity="1"
+          />
         </Container>
       )}
     </>
